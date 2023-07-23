0ff7385da0f80ed6398aa83a3bf5de58ebe583e4
a-little-dust <1429692815@qq.com>
2023-07-23 10:09:59 +0800

    finish lab1 and lab2

--- kernel/defs.h
+++ kernel/defs.h
@@ -178,6 +178,9 @@ void            plicinithart(void);
 int             plic_claim(void);
 void            plic_complete(int);
 
+//printf.c
+void backtrace(void);
+
 // virtio_disk.c
 void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
--- kernel/riscv.h
+++ kernel/riscv.h
@@ -6,6 +6,14 @@ r_mhartid()
   asm volatile("csrr %0, mhartid" : "=r" (x) );
   return x;
 }
+//指向某个函数使用的栈地址的顶端
+static inline uint64
+r_fp()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  return x;
+}
 
 // Machine Status Register, mstatus
 
