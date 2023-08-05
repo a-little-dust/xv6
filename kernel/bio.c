9528b5f0b5f6b110861867ae6ac4321ceb1af241
a-little-dust <1429692815@qq.com>
2023-08-05 17:16:17 +0800

    finish report

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,32 +23,31 @@
 #include "fs.h"
 #include "buf.h"
 
-#define NBUFMAP_BUCKET 13
+#define NBUFMAP_BUCKET 13//分成多少个桶来存储缓冲区映射关系
+//哈希函数，用于根据给定的设备号dev和块号blockno来计算对应的桶索引，从而定位到缓冲区映射表中的某个桶
 #define BUFMAP_HASH(dev, blockno) ((((dev)<<27)|(blockno))%NBUFMAP_BUCKET)
 
 struct {
-  struct buf buf[NBUF];
-  struct spinlock eviction_lock;
-
-  // Hash map: dev and blockno to buf
-  struct buf bufmap[NBUFMAP_BUCKET];
-  struct spinlock bufmap_locks[NBUFMAP_BUCKET];
-} bcache;
+  struct buf buf[NBUF]; // 缓冲区数组，NBUF是实际需要的缓冲区的数量
+  struct spinlock eviction_lock;//自旋锁，用于保护缓冲区
+  struct buf bufmap[NBUFMAP_BUCKET];// 缓冲区对应的桶，可以根据设备号和块号找到对应的桶
+  struct spinlock bufmap_locks[NBUFMAP_BUCKET];// 桶的锁，用于保护并发访问桶的操作
+} bcache;//块缓存
 
 void
 binit(void)
 {
-  // Initialize bufmap
+  // 初始化bufmap
   for(int i=0;i<NBUFMAP_BUCKET;i++) {
-    initlock(&bcache.bufmap_locks[i], "bcache_bufmap");
-    bcache.bufmap[i].next = 0;
+    initlock(&bcache.bufmap_locks[i], "bcache_bufmap");// 初始化bufmap的锁
+    bcache.bufmap[i].next = 0;// 将bufmap的next字段初始化为0
   }
 
-  // Initialize buffers
+   // 初始化buffers
   for(int i=0;i<NBUF;i++){
     struct buf *b = &bcache.buf[i];
     initsleeplock(&b->lock, "buffer");
-    b->lastuse = 0;
+    b->lastuse = 0; // 将buffer的lastuse字段初始化为0
     b->refcnt = 0;
     // put all the buffers into bufmap[0]
     b->next = bcache.bufmap[0].next;
@@ -58,38 +57,38 @@ binit(void)
   initlock(&bcache.eviction_lock, "bcache_eviction");
 }
 
-// Look through buffer cache for block on device dev.
-// If not found, allocate a buffer.
-// In either case, return locked buffer.
+//在设备dev上查找块。
+//如果没有找到，分配一个缓冲区。
+//无论哪种情况，都返回锁定的缓冲区。
 static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  uint key = BUFMAP_HASH(dev, blockno);
+  uint key = BUFMAP_HASH(dev, blockno);// 根据设备和块号计算哈希值，确定bufmap的索引位置
 
-  acquire(&bcache.bufmap_locks[key]);
+  acquire(&bcache.bufmap_locks[key]);// 获取bufmap对应的锁
 
-  // Is the block already cached?
+  // 检查是否已经缓存了该块
   for(b = bcache.bufmap[key].next; b; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(&bcache.bufmap_locks[key]);
-      acquiresleep(&b->lock);
-      return b;
+      b->refcnt++;// 引用计数加1
+      release(&bcache.bufmap_locks[key]);// 释放bufmap的锁
+      acquiresleep(&b->lock);// 获取缓冲区的睡眠锁
+      return b;// 返回被锁定的缓冲区
     }
   }
-  release(&bcache.bufmap_locks[key]);
-  acquire(&bcache.eviction_lock);
+  release(&bcache.bufmap_locks[key]);// 释放bufmap的锁
+  acquire(&bcache.eviction_lock);// 获取eviction锁，保护缓冲区的驱逐（eviction）过程
 
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
+  // 缓冲区未被缓存
+  // 回收最近未使用的未使用缓冲区
    for(b = bcache.bufmap[key].next; b; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
-      acquire(&bcache.bufmap_locks[key]); // must do, for `refcnt++`
-      b->refcnt++;
-      release(&bcache.bufmap_locks[key]);
-      release(&bcache.eviction_lock);
+      acquire(&bcache.bufmap_locks[key]); // 获取bufmap锁
+      b->refcnt++;//引用计数加1
+      release(&bcache.bufmap_locks[key]);// 释放bufmap的锁
+      release(&bcache.eviction_lock);// 释放eviction锁
       acquiresleep(&b->lock);
       return b;
     }
@@ -97,18 +96,17 @@ bget(uint dev, uint blockno)
   struct buf *before_least = 0; 
   uint holding_bucket = -1;
   for(int i = 0; i < NBUFMAP_BUCKET; i++){
-    // before acquiring, we are either holding nothing, or only holding locks of
-    // buckets that are *on the left side* of the current bucket
-    // so no circular wait can ever happen here. (safe from deadlock)
-    acquire(&bcache.bufmap_locks[i]);
+    // 在获取锁之前，要么不持有任何锁，要么只持有当前桶左侧的桶的锁
+    // 所以这里不会出现循环等待的情况（避免死锁）
+    acquire(&bcache.bufmap_locks[i]);// 获取bufmap中桶对应的锁
     int newfound = 0; // new least-recently-used buf found in this bucket
     for(b = &bcache.bufmap[i]; b->next; b = b->next) {
       if(b->next->refcnt == 0 && (!before_least || b->next->lastuse < before_least->next->lastuse)) {
         before_least = b;
         newfound = 1;
-      }
+      } // 更新最近未使用的缓冲区的前一个节点
     }
-    if(!newfound) {
+    if(!newfound) {// 如果在当前桶中没有找到最近未使用的缓冲区，则释放该锁
       release(&bcache.bufmap_locks[i]);
     } else {
       if(holding_bucket != -1) release(&bcache.bufmap_locks[holding_bucket]);
@@ -116,29 +114,29 @@ bget(uint dev, uint blockno)
       // keep holding this bucket's lock....
     }
   }
-  if(!before_least) {
+  if(!before_least) { // 如果没有找到未使用的缓冲区，则发生错误
     panic("bget: no buffers");
   }
   b = before_least->next;
   
   if(holding_bucket != key) {
-    // remove the buf from it's original bucket
+     // 从原来的桶中移除缓冲区
     before_least->next = b->next;
-    release(&bcache.bufmap_locks[holding_bucket]);
-    // rehash and add it to the target bucket
+    release(&bcache.bufmap_locks[holding_bucket]);// 释放持有的桶的锁
+    // 重新计算哈希并将其添加到目标桶中
     acquire(&bcache.bufmap_locks[key]);
     b->next = bcache.bufmap[key].next;
     bcache.bufmap[key].next = b;
   }
   
-  b->dev = dev;
-  b->blockno = blockno;
-  b->refcnt = 1;
-  b->valid = 0;
-  release(&bcache.bufmap_locks[key]);
-  release(&bcache.eviction_lock);
-  acquiresleep(&b->lock);
-  return b;
+  b->dev = dev; // 设置缓冲区的设备号
+  b->blockno = blockno; // 设置缓冲区的块号
+  b->refcnt = 1; // 引用计数设置为1，表示有一个进程使用了该缓冲区
+  b->valid = 0; // 缓冲区的内容无效
+  release(&bcache.bufmap_locks[key]); // 释放bufmap的锁
+  release(&bcache.eviction_lock); // 释放eviction锁
+  acquiresleep(&b->lock); // 获取缓冲区的睡眠锁
+  return b; // 返回被锁定的缓冲区
 }
 
 // Return a locked buf with the contents of the indicated block.
@@ -164,41 +162,38 @@ bwrite(struct buf *b)
   virtio_disk_rw(b, 1);
 }
 
-// Release a locked buffer.
-// Move to the head of the most-recently-used list.
-void
-brelse(struct buf *b)
-{
-  if(!holdingsleep(&b->lock))
+// 释放一个被锁定的缓冲区。
+void brelse(struct buf *b) {
+  if(!holdingsleep(&b->lock)) // 检查当前线程是否持有睡眠锁，如果没有持有，则发生错误
     panic("brelse");
 
-  releasesleep(&b->lock);
+  releasesleep(&b->lock); // 释放睡眠锁
 
-  uint key = BUFMAP_HASH(b->dev, b->blockno);
-
-  acquire(&bcache.bufmap_locks[key]);
-  b->refcnt--;
-  if (b->refcnt == 0) {
-    b->lastuse = ticks;
+  uint key = BUFMAP_HASH(b->dev, b->blockno); // 根据设备和块号计算哈希值，确定bufmap的索引位置
+  acquire(&bcache.bufmap_locks[key]); // 获取bufmap对应的锁
+  b->refcnt--; // 引用计数减1，表示当前线程不再使用该缓冲区
+  if (b->refcnt == 0) { // 如果引用计数为0，则表示缓冲区没有被其他线程使用
+    b->lastuse = ticks; // 更新最后使用时间为当前系统滴答数
   }
-  release(&bcache.bufmap_locks[key]);
+  release(&bcache.bufmap_locks[key]); // 释放bufmap的锁
+
 }
 
-void
-bpin(struct buf *b) {
-  uint key = BUFMAP_HASH(b->dev, b->blockno);
+// 将缓冲区锁定，使其不能被回收。
+void bpin(struct buf *b) {
+  uint key = BUFMAP_HASH(b->dev, b->blockno); // 根据设备和块号计算哈希值，确定bufmap的索引位置
 
-  acquire(&bcache.bufmap_locks[key]);
-  b->refcnt++;
-  release(&bcache.bufmap_locks[key]);
+  acquire(&bcache.bufmap_locks[key]); // 获取bufmap对应的锁
+  b->refcnt++; // 引用计数加1，表示有一个进程正在使用该缓冲区
+  release(&bcache.bufmap_locks[key]); // 释放bufmap的锁
 }
 
-void
-bunpin(struct buf *b) {
-  uint key = BUFMAP_HASH(b->dev, b->blockno);
+// 取消对缓冲区的锁定，允许其被回收。
+void bunpin(struct buf *b) {
+  uint key = BUFMAP_HASH(b->dev, b->blockno); // 根据设备和块号计算哈希值，确定bufmap的索引位置
 
-  acquire(&bcache.bufmap_locks[key]);
-  b->refcnt--;
-  release(&bcache.bufmap_locks[key]);
+  acquire(&bcache.bufmap_locks[key]); // 获取bufmap对应的锁
+  b->refcnt--; // 引用计数减1，表示有一个进程不再使用该缓冲区
+  release(&bcache.bufmap_locks[key]); // 释放bufmap的锁
 }
 
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,19 +23,19 @@ struct run {
 //   struct run *freelist;
 // } kmem;
 struct {
-  struct spinlock lock;
-  struct run *freelist;
-  char lock_name[7];
-} kmem[NCPU];//定义NCPU个kmem结构体
+  struct spinlock lock;//锁
+  struct run *freelist;// 空闲资源链表
+  char lock_name[7];// 锁的名称
+} kmem[NCPU];//定义NCPU个kmem结构体,用于管理不同CPU的内存分配
 
 void
-kinit()//对锁进行初始化
+kinit()//初始化内存
 {
-  for (int i = 0; i < NCPU; i++) {
-    snprintf(kmem[i].lock_name, sizeof(kmem[i].lock_name), "kmem_%d", i);
-    initlock(&kmem[i].lock, kmem[i].lock_name);
+  for (int i = 0; i < NCPU; i++) {//给每个CPU设置锁
+    snprintf(kmem[i].lock_name, sizeof(kmem[i].lock_name), "kmem_%d", i);// 格式化锁的名称
+    initlock(&kmem[i].lock, kmem[i].lock_name);// 初始化锁
   }
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void*)PHYSTOP);// 初始化空闲资源
 }
 
 void
@@ -54,82 +54,78 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
-
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  struct run *r;//用来指向要释放的内存块
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)// 检查释放的内存块是否合法
     panic("kfree");
-
-  // Fill with junk to catch dangling refs.
+  // 将内存块填充为垃圾值，以捕获悬空引用
   memset(pa, 1, PGSIZE);
-
   r = (struct run*)pa;
-  push_off();
-  int id = cpuid();
+  push_off();// 关闭中断，禁止CPU切换
+  int id = cpuid();// 获取当前CPU的ID
 
-  acquire(&kmem[id].lock);
-  r->next = kmem[id].freelist;
-  kmem[id].freelist = r;
-  release(&kmem[id].lock);
+  acquire(&kmem[id].lock);//获取当前CPU对应的锁，确保对空闲资源链表的互斥访问
+  r->next = kmem[id].freelist;//将释放的内存块插入到当前CPU的空闲资源链表头部
+  kmem[id].freelist = r;//更新当前CPU的空闲资源链表头指针
+  release(&kmem[id].lock);//释放当前CPU对应的锁
 
-  pop_off();
+  pop_off();//打开中断，允许CPU切换
 }
 
-// Allocate one 4096-byte page of physical memory.
-// Returns a pointer that the kernel can use.
-// Returns 0 if the memory cannot be allocated.
+//分配一个4096字节的物理内存页。
+//返回一个内核可以使用的指针。
+//如果无法分配内存，则返回0。
 void *
 kalloc(void)
 {
   struct run *r;
 
-  push_off();
+  push_off();// 关闭中断，禁止CPU切换
   int id = cpuid();
 
-  acquire(&kmem[id].lock);
-  r = kmem[id].freelist;
-  if(r) {
+  acquire(&kmem[id].lock);// 获取当前CPU对应的锁，确保互斥操作
+  r = kmem[id].freelist;// 将当前CPU空闲资源链表的头节点赋值给r
+  if(r) {// 如果空闲资源链表非空，将头节点指向下一个节点
     kmem[id].freelist = r->next;
   }
-  else {
-    // alloc failed, try to steal from other cpu
+  else {// 分配失败，尝试从其他CPU上窃取
     int success = 0;
     int i = 0;
     for(i = 0; i < NCPU; i++) {
       if (i == id) continue;
-      acquire(&kmem[i].lock);
-      struct run *p = kmem[i].freelist;
+      acquire(&kmem[i].lock);// 获取其他CPU对应的锁
+      struct run *p = kmem[i].freelist; // 将其他CPU空闲资源链表的头节点赋值给p
       if(p) {
-        // steal half of memory
-        struct run *fp = p; // faster pointer
-        struct run *pre = p;
-        while (fp && fp->next) {
+        // 窃取一半的内存
+        struct run *fp = p; // 快速指针
+        struct run *pre = p;//p的前一个
+        while (fp && fp->next) {//fp 指针每次向后移动两个节点，而 p 指针和 pre 指针则每次向后移动一个节点
           fp = fp->next->next;
           pre = p;
           p = p->next;
-        }
-        kmem[id].freelist = kmem[i].freelist;
+        }//当 fp 到达链表末尾时，p 正好位于链表的中间位置。同时，pre 指针是 p 的前一个节点。
+        kmem[id].freelist = kmem[i].freelist; // 将窃取的一半内存分配给当前CPU
         if (p == kmem[i].freelist) {
-          // only have one page
+          // 只有一页内存可用，将其他CPU空闲资源链表置为0
           kmem[i].freelist = 0;
         }
-        else {
-          kmem[i].freelist = p;
-          pre->next = 0;
+        else {//其他CPU还有空闲资源可用
+          kmem[i].freelist = p; // 更新其他CPU空闲资源链表的头指针
+          pre->next = 0;// 断开链表
         }
-        success = 1;
+        success = 1;//成功
       }
-      release(&kmem[i].lock);
+      release(&kmem[i].lock); // 释放其他CPU对应的锁
       if (success) {
-        r = kmem[id].freelist;
-        kmem[id].freelist = r->next;
+        r = kmem[id].freelist;// 将当前CPU空闲资源链表的头节点赋值给r
+        kmem[id].freelist = r->next;// 更新当前CPU空闲资源链表的头指针
         break;
       }
     }
   }
-  release(&kmem[id].lock);
-  pop_off();
+  release(&kmem[id].lock);// 释放当前CPU对应的锁
+  pop_off();// 打开中断，允许CPU切换
 
   if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    memset((char*)r, 5, PGSIZE); // 填充垃圾值到分配的内存块
+  return (void*)r;// 返回分配的内存块的指针
 }
